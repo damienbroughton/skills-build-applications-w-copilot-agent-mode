b19254ae17f2052ce42e1ac8c4dd97953a49c986
damienbroughton <damienbroughton93@gmail.com>
2025-04-08 03:35:14 +0000

    Update settings and views for improved security and API response structure
    - Restrict ALLOWED_HOSTS to specific domains for better security.
    - Simplify api_root view to only handle GET requests and update response URLs to use the base URL.

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-g*sxyh*n!)$g56uw+z7ds82vnu6*z00mpnr)0w$_vc##(25w7i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'special-broccoli-g4667q99q5pwhppp9-8000.app.github.dev']
 
 
 # Application definition
